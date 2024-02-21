cc0e21b93b2bd022d1b79c68dd5fb1e358c0f1df
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-02-21 18:09:04 +0100

    use uv to update reqs

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -3,14 +3,11 @@
 # Pin current dependencies versions.
 #
 
-rm -f requirements.txt
-rm -f requirements.dev.txt
-
 # pin test / lint / docs dependencies for reproducibility
-pip-compile requirements.dev.in
+uv pip compile requirements.dev.in
 
 # pin requirements.in versions just as reference for potential incapability bugs in future
-pip-compile requirements.in
+uv pip compile requirements.in
 
 # do not pin dependencies in the package
 scripts/include_pyproject_requirements.py requirements.in
