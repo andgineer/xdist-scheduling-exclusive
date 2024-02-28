93a8932220ae21c8dc7972b30a8fd456cb6955ed
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-02-28 21:48:21 +0100

    removed unnecessary lint suppressing

--- src/xdist_scheduling_exclusive/__init__.py
+++ src/xdist_scheduling_exclusive/__init__.py
@@ -2,6 +2,8 @@
 
 Can significantly improve runtime by running long tests on separate workers.
 """
-from xdist_scheduling_exclusive.exclusive_load_scheduling import ExclusiveLoadScheduling  # noqa
-from xdist_scheduling_exclusive.exclusive_loadfile_scheduling import ExclusiveLoadFileScheduling  # noqa
-from xdist_scheduling_exclusive.exclusive_loadscope_scheduling import ExclusiveLoadScopeScheduling  # noqa
+from xdist_scheduling_exclusive.exclusive_load_scheduling import ExclusiveLoadScheduling
+from xdist_scheduling_exclusive.exclusive_loadfile_scheduling import ExclusiveLoadFileScheduling
+from xdist_scheduling_exclusive.exclusive_loadscope_scheduling import ExclusiveLoadScopeScheduling
+
+__all__ = ["ExclusiveLoadScheduling", "ExclusiveLoadFileScheduling", "ExclusiveLoadScopeScheduling"]
